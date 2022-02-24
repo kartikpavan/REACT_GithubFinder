ba9c96321981ba5788e636f8ff4779d2a2c72f8a
kartikpavan <kartik.varanasi@yahoo.com>
2022-02-24 22:34:05 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
         <main>Content</main>
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer class="p-2 bg-orange-400/70 footer footer-center text-white">
+      <div>
+        <p>
+          Copyright © 2022 - All right reserved by
+          <a href="https://github.com/kartikpavan">Divya Kartik Pavan</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
