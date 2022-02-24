ac26ce0a6ceca95b99bac9fa97a24ebed186b3b0
kartikpavan <kartik.varanasi@yahoo.com>
2022-02-24 22:23:10 +0530

    Navbar with Daisy UI and tailwind

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,35 @@
+import { FaGithub } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+function Navbar() {
+  return (
+    <nav className="navbar mb-12 shadow-lg bg-orange-400/70 text-xl font-bold text-white  ">
+      <div className="container mx-auto">
+        <div className="flex-none px-2 mx-2">
+          <FaGithub size="40" className="inline pr-2 text-3xl" />
+          <Link to="/">Github Finder</Link>
+        </div>
+        <div className="flex-1 px-2 mx-2">
+          <div className="flex justify-end">
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+              Home
+            </Link>
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+Navbar.defaultProps = {
+  title: 'Github Finder',
+};
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Navbar;
